757f64919fe6349380706489c95039b032fa6265
Elnaz Pashaei <145416583+nazpashaei@users.noreply.github.com>
2025-07-16 02:47:16 -0400

    Update DiCE_function.R

--- R/DiCE_function.R
+++ R/DiCE_function.R
@@ -22,7 +22,7 @@
 #' @export
 
 
-DiCE_function <- function(data,regulation_status,species,method,pval_threshold, log2fc_threshold){
+DiCE_function <- function(data,regulation_status,species,method,pval_threshold, log2fc_threshold,pval_type){
 
   library(dplyr)
   library(tibble)
@@ -62,16 +62,21 @@ DiCE_function <- function(data,regulation_status,species,method,pval_threshold, 
 
   #Phase I: Construction of a candidate gene pool by DEA with a loose cutoff
   #=======================================
- if (regulation_status == "Up") {
-  dee = data$DE[(data$DE$adj.P.Val <= pval_threshold) & (data$DE$logFC >= log2fc_threshold), ]
+  if (!(pval_type %in% c("adj.P.Val", "P.Value"))) {
+  stop("Invalid pval_type. Use 'adj.P.Val' or 'P.Value'.")
+}
+pvals <- data$DE[[pval_type]]
+
+if (regulation_status == "Up") {
+  dee <- data$DE[(pvals <= pval_threshold) & (data$DE$logFC >= log2fc_threshold), ]
 } else if (regulation_status == "Down") {
-  dee = data$DE[(data$DE$adj.P.Val <= pval_threshold) & (data$DE$logFC <= -log2fc_threshold), ]
+  dee <- data$DE[(pvals <= pval_threshold) & (data$DE$logFC <= -log2fc_threshold), ]
 } else {
-  dee = data$DE[(data$DE$adj.P.Val <= pval_threshold) & (abs(data$DE$logFC) >= log2fc_threshold), ]
+  dee <- data$DE[(pvals <= pval_threshold) & (abs(data$DE$logFC) >= log2fc_threshold), ]
 }
    
   dee1=dee
-  colnames(dee1)=c("gene_name","P.Value","logFC")
+  colnames(dee1)=c("gene_name","P.Value","adj.P.Val","logFC")
 
   #+++++++++++++++++++++++Phase II: Selection of the top discriminative genes from the candidate pool obtained in Phase I using the Information Gain (IG) filter approach.
 
