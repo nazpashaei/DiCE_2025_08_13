86a1bf5053326d95028ec9c42d502cc6186bc199
Elnaz Pashaei <145416583+nazpashaei@users.noreply.github.com>
2025-09-16 13:42:24 -0400

    Update DiCE_function.R

--- R/DiCE_function.R
+++ R/DiCE_function.R
@@ -211,6 +211,8 @@ if (regulation_status == "Up") {
   g <- graph_from_data_frame(sss,directed = FALSE)
   is_weighted(g)
   g <- set_edge_attr(g, "weight", value=(1-(sss$weight))+0.001)
+  g1 <- set_edge_attr(g, "weight", value=sss$weight)
+
   E(g)$weight[is.na(E(g)$weight)] <- 1
 
   V(g); E(g)
@@ -219,7 +221,7 @@ if (regulation_status == "Up") {
   is_weighted(g)
   #Phase IV: Topological analysis of sample-specific weighted PPI networks from the perspectives of Betweenness and Eigenvector centrality, followed by differential analysis on each centrality measure of individual genes.
   Strength=sort(strength(g),decreasing = TRUE);rank=seq(1,length(Strength),1);Strength=cbind(Strength,rank);#View(Strength)
-  centralities <- cbind(Eig=evcent(g)$vector,Betweenness=betweenness(g));#View(centralities)
+  centralities <- cbind(Eig=evcent(g1)$vector,Betweenness=betweenness(g));#View(centralities)
 
   centralities.Normal <-centralities
   #***********************************
@@ -252,13 +254,15 @@ if (regulation_status == "Up") {
   g <- graph_from_data_frame(sss,directed = FALSE)
   is_weighted(g)
   g <- set_edge_attr(g, "weight", value=(1-(sss$weight))+0.001)
+  g1 <- set_edge_attr(g, "weight", value=sss$weight)
+
   E(g)$weight[is.na(E(g)$weight)] <- 1
   V(g); E(g)
   gsize(g)
   gorder(g)
   is_weighted(g)
   Strength=sort(strength(g),decreasing = TRUE);rank=seq(1,length(Strength),1);Strength=cbind(Strength,rank);#View(Strength)
-  centralities <- cbind(Eig=evcent(g)$vector,Betweenness=betweenness(g));#View(centralities)
+  centralities <- cbind(Eig=evcent(g1)$vector,Betweenness=betweenness(g));#View(centralities)
 
   centralities.Tumor <-centralities
 
